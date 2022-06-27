dc67635f8389b279257ede52363149650d5fa5ff
Izznakim <IzzuddinNaufalHakim@gmail.com>
2022-06-27 17:02:17 +0700

    TODO 2 Not solve YET

--- app/src/main/java/com/dicoding/courseschedule/data/CourseDao.kt
+++ app/src/main/java/com/dicoding/courseschedule/data/CourseDao.kt
@@ -2,21 +2,28 @@ package com.dicoding.courseschedule.data
 
 import androidx.lifecycle.LiveData
 import androidx.paging.DataSource
+import androidx.room.*
 import androidx.sqlite.db.SupportSQLiteQuery
 
 //TODO 2 : Define data access object (DAO)
+@Dao
 interface CourseDao {
-
+    @RawQuery(observedEntities = [Course::class])
     fun getNearestSchedule(query: SupportSQLiteQuery): LiveData<Course?>
 
+    @RawQuery(observedEntities = [Course::class])
     fun getAll(query: SupportSQLiteQuery): DataSource.Factory<Int, Course>
 
+    @Query("SELECT * FROM course WHERE id = :id")
     fun getCourse(id: Int): LiveData<Course>
 
+    @Query("SELECT * FROM course WHERE day = :day")
     fun getTodaySchedule(day: Int): List<Course>
 
+    @Insert
     fun insert(course: Course)
 
+    @Delete
     fun delete(course: Course)
 
     fun sort(params: String): DataSource.Factory<Int, Course>
