8aaaedc55440eda0c68a10bd9b3d48bb14c4a36e
Izznakim <IzzuddinNaufalHakim@gmail.com>
2022-06-28 18:50:12 +0700

    TODO 10 SOLVED.

--- app/src/main/java/com/dicoding/courseschedule/ui/setting/SettingsFragment.kt
+++ app/src/main/java/com/dicoding/courseschedule/ui/setting/SettingsFragment.kt
@@ -1,21 +1,54 @@
 package com.dicoding.courseschedule.ui.setting
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
 import com.dicoding.courseschedule.R
+import com.dicoding.courseschedule.util.NightMode
 
-class SettingsFragment : PreferenceFragmentCompat() {
+class SettingsFragment : PreferenceFragmentCompat(),
+    SharedPreferences.OnSharedPreferenceChangeListener {
+
+    private lateinit var THEME: String
+
+    private lateinit var themePreference: ListPreference
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
-        //TODO 10 : Update theme based on value in ListPreference
+        //TODO 10 : Update theme based on value in ListPreference [SOLVED]
+        init()
         //TODO 11 : Schedule and cancel notification in DailyReminder based on SwitchPreference
+    }
+
+    private fun init() {
+        THEME = resources.getString(R.string.pref_key_dark)
+
+        themePreference = findPreference<ListPreference>(THEME) as ListPreference
+    }
+
+    override fun onResume() {
+        super.onResume()
+        preferenceScreen.sharedPreferences?.registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        preferenceScreen.sharedPreferences?.unregisterOnSharedPreferenceChangeListener(this)
     }
 
     private fun updateTheme(nightMode: Int): Boolean {
         AppCompatDelegate.setDefaultNightMode(nightMode)
         requireActivity().recreate()
         return true
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) {
+        if (key == THEME) {
+            val mode=NightMode.valueOf(themePreference.value.toUpperCase())
+            updateTheme(mode.value)
+        }
     }
 }
