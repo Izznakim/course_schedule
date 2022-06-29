57157d9ce2f8d3eb8b9d5475e827b9f146a84f9b
Izznakim <IzzuddinNaufalHakim@gmail.com>
2022-06-29 07:43:55 +0700

    TODO 11 SOLVED.

--- app/src/main/java/com/dicoding/courseschedule/ui/setting/SettingsFragment.kt
+++ app/src/main/java/com/dicoding/courseschedule/ui/setting/SettingsFragment.kt
@@ -6,27 +6,35 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreference
 import com.dicoding.courseschedule.R
+import com.dicoding.courseschedule.notification.DailyReminder
 import com.dicoding.courseschedule.util.NightMode
-
 class SettingsFragment : PreferenceFragmentCompat(),
     SharedPreferences.OnSharedPreferenceChangeListener {
 
     private lateinit var THEME: String
+    private lateinit var NOTIFICATION: String
 
     private lateinit var themePreference: ListPreference
+    private lateinit var notificationPreference: SwitchPreference
+
+    private val AKTIF="AKTIF"
+    private val NONAKTIF="NONAKTIF"
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
         //TODO 10 : Update theme based on value in ListPreference [SOLVED]
+        //TODO 11 : Schedule and cancel notification in DailyReminder based on SwitchPreference [SOLVED]
         init()
-        //TODO 11 : Schedule and cancel notification in DailyReminder based on SwitchPreference
     }
 
     private fun init() {
         THEME = resources.getString(R.string.pref_key_dark)
+        NOTIFICATION = resources.getString(R.string.pref_key_notify)
 
         themePreference = findPreference<ListPreference>(THEME) as ListPreference
+        notificationPreference = findPreference<SwitchPreference>(NOTIFICATION) as SwitchPreference
     }
 
     override fun onResume() {
@@ -47,8 +55,20 @@ class SettingsFragment : PreferenceFragmentCompat(),
 
     override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) {
         if (key == THEME) {
-            val mode=NightMode.valueOf(themePreference.value.toUpperCase())
+            val mode = NightMode.valueOf(themePreference.value.toUpperCase())
             updateTheme(mode.value)
+        }
+
+        if (key == NOTIFICATION) {
+            val dailyReminder=DailyReminder()
+            if (notificationPreference.isChecked){
+                Log.d("SettingsFragment", "onSharedPreferenceChanged: ${notificationPreference.switchTextOn}")
+                dailyReminder.setDailyReminder(requireContext())
+            }else{
+                Log.d("SettingsFragment", "onSharedPreferenceChanged: ${notificationPreference.switchTextOff}")
+                dailyReminder.cancelAlarm(requireContext())
+            }
         }
     }
 }
+
